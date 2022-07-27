9cb7c23df8a78cd4afe17099b0e31ec015d648fd
WZY009 <2232213966@qq.com>
2022-07-27 20:18:05 +0800

    July 27th

---
+++ StackAndQueue/Priority Queue/295. Find Median from Data Stream/lc295.cpp
@@ -0,0 +1,38 @@
+class MedianFinder {
+public:
+    priority_queue<int, vector<int>, less<int>> queMin;  // the number which is smaller or equal than the median.
+    priority_queue<int, vector<int>, greater<int>> queMax; // the number which is larger than the median.
+
+    MedianFinder() {}
+
+    void addNum(int num) {
+        if (queMin.empty() || num <= queMin.top()) {
+            queMin.push(num);
+            if (queMax.size() + 1 < queMin.size()) {
+                queMax.push(queMin.top());
+                queMin.pop();
+            }
+        } else {
+            queMax.push(num);
+            if (queMax.size() > queMin.size()) {
+                queMin.push(queMax.top());
+                queMax.pop();
+            }
+        }
+    }
+
+    double findMedian() {
+        if (queMin.size() > queMax.size()) {
+            return queMin.top();
+        }
+        return ((double)queMin.top() + (double)queMax.top()) / 2.0;
+    }
+};
+
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
